c34b1b14edb94703a8f5b8d2620d4c4bec6130ab
Неджад <mesh.nedzhad@gmail.com>
2021-08-22 17:16:25 +0300

    Домашнее задание № 1 по курсу "ASP.NET Core Веб Приложение"

---
+++ Lesson_1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Lesson_1
+{
+    class Program
+    {
+        private HttpClient Client = new HttpClient();
+        
+        public static async Task Main(string[] args)
+        {
+            Program program = new Program();
+
+            for (int i = 4; i <= 13; i++)
+            {
+                var task = new[]
+                {
+                    program.GetItems(i),
+                };
+
+                try
+                {
+                    var tasks = await Task.WhenAny(task);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
+        }
+
+        private async Task GetItems(int id)
+        {
+            string response = await Client.GetStringAsync($"https://jsonplaceholder.typicode.com/posts/{id}");
+            Console.WriteLine(response);
+            using (StreamWriter writer = new StreamWriter("path.txt", true))
+            {
+                await writer.WriteLineAsync(response);
+            }
+        }
+        
+        
+    }
+}
